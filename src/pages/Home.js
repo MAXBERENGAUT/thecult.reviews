b7722f219a79ec3bfd11b0237df7052253955452
Josh Devine <joshuaddevine@gmail.com>
2022-05-17 22:31:09 -0400

    added full capabilities to react version of site

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,148 +1,124 @@
-import { Component, createRef } from 'react'
+import React, { useState, useRef } from 'react'
 import Reviews from './reviews.json'
-import Background from './media/timelapse.mp4'
-import YTLogo from './media/yt_logo.svg'
 import './Home.css';
 
+const ITEM_COUNT_MIN = 20
 const ITEM_WIDTH = 300
 const ITEM_MARGIN = 30
 const ITEM_BORDER = 2
 const ITEM_LENGTH = ITEM_WIDTH + 2*(ITEM_MARGIN + ITEM_BORDER)
 
-const COVER_BASE_URL = 'https://ik.imagekit.io/maxberengautsites/covers/'
-const LINK_BASE_URL = 'https://youtu.be/'
+const URL_BASE_IMAGEKIT = 'https://ik.imagekit.io/maxberengautsites/'
+const URL_BASE_LINK = 'https://youtu.be/'
+const URL_BASE_COVER = URL_BASE_IMAGEKIT + 'covers/'
+const URL_BACKGROUND = URL_BASE_IMAGEKIT + 'thecult_reviews/timelapse.mp4'
+const URL_YT_LOGO = URL_BASE_IMAGEKIT + 'thecult_reviews/yt_logo.svg'
 
-class Menu extends Component {    
-    constructor(props) {
-        super(props)
 
-        this.state = { 
-            offset: 0,
-            distance: 0
+function Menu() {    
+    const [pos, setPos] = useState({distance: 0, offset: 0})
+    const [items, setItems] = useState(() => {
+        // pad reviews to fill screen
+        while (Reviews.length < ITEM_COUNT_MIN) {
+            Reviews = Reviews.concat(Reviews)
         }
 
-        this.ref = createRef()
-    }
-
-    onMouseMove = e => {
-        if (!this.isDown) return;
-
-        e.preventDefault();
-        let x2 = e.pageX - this.ref.current.offsetLeft
-        let d = this.ref.current.scrollLeft - (this.x1 - x2);
-
-        if (this.state.distance + d > 0) {
-            d = -this.state.distance;
-        }
-
-        this.setState({ 
-            offset: this.state.offset + d,
-            distance: this.state.distance + d
-        });
-            
-        this.x1 = e.pageX - this.ref.current.offsetLeft
-        this.wrapItems()
-    }
-
-    onMouseDown = e => { 
-        e.persist()
-        this.isDown = true
-    
-        this.x1 = e.pageX - this.ref.current.offsetLeft
-        this.ref.current.style.cursor = "grabbing"
-    }
-
-    onMouseUp = e => { 
-        this.isDown = false
-        this.ref.current.style.cursor = "unset"
-    }
-
-    reposition = () => {
-        return {
-            transform: `translateX(${this.state.offset}px)`
-        }
-    }
-
-    items = Reviews.map( (review, index) => {
-        return (
-            <div className='item highlight' key={index}>
-                <img 
-                    className='cover tilt' 
-                    src={COVER_BASE_URL + review.cover} 
-                    alt={review.album + ' cover'}
-                />
-                <p className='song'>{review.song}</p>
-                <p className='artist'>{review.artist}</p>
-                <p className='album'>{review.album}</p>
-                <p className='year'>{review.year}</p>
-                <a href={LINK_BASE_URL + review.link}>
+        return Reviews.map((review, index) => {
+            return (
+                <div className='item' key={index}>
                     <img 
-                        className='yt-logo'
-                        src={YTLogo}
-                        alt='YouTube logo'
+                        className='cover tilt' 
+                        src={URL_BASE_COVER + review.cover} 
+                        alt={review.album + ' cover'}
                     />
-                </a>
-            </div>
-        )
-    })
+                    <p className='song'>{review.song}</p>
+                    <p className='artist'>{review.artist}</p>
+                    <p className='album'>{review.album}</p>
+                    <p className='year'>{review.year}</p>
+                    <a href={URL_BASE_LINK + review.link}>
+                        <img 
+                            className='yt-logo'
+                            src={URL_YT_LOGO}
+                            alt='YouTube logo'
+                        />
+                    </a>
+                </div>
+            )}
+        )}
+    )
 
-    wrapItems = () => {
+    const menu = useRef(null)
+    const x1 = useRef(null)
+    const isDown = useRef(false)
+
+    function onMouseMove(e) {
+        if (!isDown.current) return;
+
+        e.preventDefault()
+        let x2 = e.pageX - menu.current.offsetLeft
+        let d = Math.min(menu.current.scrollLeft - (x1.current - x2), -pos.distance)
+        x1.current = e.pageX - menu.current.offsetLeft
+
+        setPos({ distance: pos.distance + d, offset: pos.offset + d })
+
         // if we have moved far enough left, wrap front item to back
-        if (-this.state.offset > ITEM_LENGTH) {
-            this.items.push(this.items[0])
-            this.items.shift()
-            
-            this.setState({ 
-                offset: this.state.offset + ITEM_LENGTH,
-                distance: this.state.distance
-            })
+        if (-pos.offset > ITEM_LENGTH) {
+            setItems([...items.slice(1), items[0]])
+            setPos({ distance: pos.distance, offset: pos.offset + ITEM_LENGTH })
         }
         
         // if we have moved far enough right, wrap back item to front
-        if (this.state.offset > 0) {
-            this.items.unshift(this.items[this.items.length-1])
-            this.items.pop()
-
-            this.setState({
-                offset: this.state.offset - ITEM_LENGTH,
-                distance: this.state.distance
-            })
+        if (pos.offset > 0) {
+            setItems([items.at(-1), ...items.slice(0, -1)])
+            setPos({ distance: pos.distance, offset: pos.offset - ITEM_LENGTH })
         }
     }
+
+    function onMouseDown(e) { 
+        e.persist()
+        isDown.current = true
+        x1.current = e.pageX - menu.current.offsetLeft
+        menu.current.style.cursor = "grabbing"
+    }
+
+    function onMouseUp() { 
+        isDown.current = false
+        menu.current.style.cursor = "unset"
+    }
+
+    function onMouseLeave() {
+        isDown.current = false
+        menu.current.style.cursor = "unset"
+    }
+
+    return (
+        <div 
+            ref={menu}
+            id='menu' 
+            style={{transform: `translateX(${pos.offset}px)`}}
+            onMouseDown={onMouseDown}
+            onMouseUp={onMouseUp}
+            onMouseMove={onMouseMove}
+            onMouseLeave={onMouseLeave}
+        >
+            {items}
+        </div>
+    )
     
-    padItems = () => {
-        console.log('padded!')
-        // while the screen is wider than the menu, double the items
-        while (this.ref.current.clientWidth + this.state.offset - ITEM_LENGTH < window.innerWidth) {
-            this.items = this.items.concat(this.items)
-        }
-    }
-
-    useEffect = () => {
-        window.addEventListener('resize', this.padItems)
-        this.padItems()
-    }
-
-    render() {
-        return (
-            <div 
-                ref={this.ref}
-                id='menu' 
-                style={this.reposition()}
-                onMouseDown={this.onMouseDown}
-                onMouseUp={this.onMouseUp}
-                onMouseMove={this.onMouseMove}
-            >
-                {this.items}
-            </div>
-        )
-    }
 }
 
-const Home = () => {
+
+function Home() {
     return (
         <div className="container">
-            <video autoPlay muted loop id='background' src={Background} type='video/mp4' />
+            <video 
+                autoPlay 
+                muted 
+                loop 
+                id='background' 
+                src={URL_BACKGROUND} 
+                type='video/mp4' 
+            />
             <Menu />
         </div>
     )
