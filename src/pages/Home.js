daddac1d61f262fce6479c1a938ed73ffa5aa6c5
Josh Devine <joshuaddevine@gmail.com>
2022-05-28 20:17:34 -0400

    Old movement 95% implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Reviews from './reviews.json'
 import './Home.css';
 
@@ -6,12 +6,13 @@ const ITEM_COUNT_MIN = 20
 const ITEM_WIDTH = 300
 const ITEM_MARGIN = 30
 const ITEM_BORDER = 2
-// const ITEM_BORDER = 0
 const ITEM_LENGTH = ITEM_WIDTH + 2*(ITEM_MARGIN + ITEM_BORDER)
 
 const MENU_INIT_OFFSET = window.innerWidth / 3
 const MENU_FRICTION = 0.95
 const MENU_MIN_VELOCITY = 0.001
+const MENU_MAX_VELOCITY = 1
+const MENU_DEAD_WIDTH = 0.2
 const MENU_UPDATE_TIME = 10
 
 const URL_BASE_IMAGEKIT = 'https://ik.imagekit.io/maxberengautsites/'
@@ -19,6 +20,54 @@ const URL_BASE_LINK = 'https://youtu.be/'
 const URL_BASE_COVER = URL_BASE_IMAGEKIT + 'covers/'
 const URL_YT_LOGO = URL_BASE_IMAGEKIT + 'thecult_reviews/yt_logo.svg'
 const URL_BACKGROUND = 'https://static.videezy.com/system/resources/previews/000/039/223/original/51_25_08_19.mp4'
+
+
+function Mouse() {
+    const [mouse, setMouse] = useState({x: 0, y: 0, isPresent: false})
+  
+    useEffect(() => {
+        function onMouseMove(e) {
+            setMouse({x: e.screenX, y: e.screenY, isPresent: true})
+        }
+
+        function onMouseLeave(e) {
+            setMouse({x: e.screenX, y: e.screenY, isPresent: false})
+        }
+
+        window.addEventListener('mousemove', onMouseMove);
+        window.addEventListener('mouseleave', onMouseLeave);
+        return () => {
+            window.removeEventListener('mousemove', onMouseMove)
+            window.removeEventListener('mouseleave', onMouseLeave)
+        }
+    }, [])
+  
+    return mouse
+}
+
+
+function Review(review, index) {
+    return (
+        <div className='item' key={index}>
+            <img 
+                className='cover tilt' 
+                src={URL_BASE_COVER + review.cover} 
+                alt={review.album + ' cover'}
+            />
+            <p className='song'>{review.song}</p>
+            <p className='artist'>{review.artist}</p>
+            <p className='album'>{review.album}</p>
+            <p className='year'>{review.year}</p>
+            <a href={URL_BASE_LINK + review.yt_id}>
+                <img 
+                    className='yt-logo'
+                    src={URL_YT_LOGO}
+                    alt='YouTube logo'
+                />
+            </a>
+        </div>
+    )
+}
 
 
 function Menu() {
@@ -34,44 +83,41 @@ function Menu() {
             Reviews = Reviews.concat(Reviews)
         }
 
-        return Reviews.map((review, index) => {
-            return (
-                <div className='item' key={index}>
-                    <img 
-                        className='cover tilt' 
-                        src={URL_BASE_COVER + review.cover} 
-                        alt={review.album + ' cover'}
-                    />
-                    <p className='song'>{review.song}</p>
-                    <p className='artist'>{review.artist}</p>
-                    <p className='album'>{review.album}</p>
-                    <p className='year'>{review.year}</p>
-                    <a href={URL_BASE_LINK + review.yt_id}>
-                        <img 
-                            className='yt-logo'
-                            src={URL_YT_LOGO}
-                            alt='YouTube logo'
-                        />
-                    </a>
-                </div>
-            )}
-        )}
-    )
+        return Reviews.map(Review)
+    })
 
+    const mouse = Mouse()
+    const isMousePresent = useRef(false)
     const menu = useRef(null)
-    const x1 = useRef(null)
-    const isDown = useRef(false)
+    
+    // check mouse positon and update menu accordingly
+    let v = 0
+    if (isMousePresent.current) {
+        const width = window.innerWidth
 
-    if (pos.velocity !== 0 && !isDown.current) {
-        let v = (Math.abs(pos.velocity) > MENU_MIN_VELOCITY) ? pos.velocity : 0
-        let d = Math.min(menu.current.scrollLeft + v, -pos.distance)
-        console.log(v)
+        let p = 0
+        if (mouse.x <= width / 2 - MENU_DEAD_WIDTH * width) {
+            p = (width/2 - MENU_DEAD_WIDTH * width - mouse.x) / (width / 2 - MENU_DEAD_WIDTH * width)
+        } else if (mouse.x >= width / 2 + MENU_DEAD_WIDTH * width) {
+            p = - (mouse.x - (width / 2 + MENU_DEAD_WIDTH * width)) / (width / 2 - MENU_DEAD_WIDTH * width)
+
+        }
+
+        v = pos.velocity + (MENU_MAX_VELOCITY * p - pos.velocity) * (1 - MENU_FRICTION)
+
+    } else {
+        v = pos.velocity * MENU_FRICTION
+    }
+    
+    // update position if velocity is non-zero
+    if (Math.abs(v) > MENU_MIN_VELOCITY) {
+        const d = Math.min(menu.current.scrollLeft + v * MENU_UPDATE_TIME, -pos.distance)
 
         setTimeout(() => {
             setPos({ 
                 distance: pos.distance + d, 
                 offset: pos.offset + d,
-                velocity: v * MENU_FRICTION
+                velocity: v
             })
             wrapItems()
         }, MENU_UPDATE_TIME)
@@ -99,44 +145,12 @@ function Menu() {
         }
     }
 
-    function onMouseMove(e) {
-        if (!isDown.current) return;
-
-        e.preventDefault()
-        let x2 = e.pageX - menu.current.offsetLeft
-        let d = Math.min(menu.current.scrollLeft - (x1.current - x2), -pos.distance)
-        x1.current = e.pageX - menu.current.offsetLeft
-
-        setPos({ 
-            distance: pos.distance + d, 
-            offset: pos.offset + d,
-            velocity: d
-        })
-
-        wrapItems()
-    }
-
-    function onMouseDown(e) { 
-        e.persist()
-        isDown.current = true
-        x1.current = e.pageX - menu.current.offsetLeft
-        menu.current.style.cursor = "grabbing"
-    }
-
-    function onMouseUp() { 
-        isDown.current = false
-        menu.current.style.cursor = "unset"
-
-        // setPos({
-        //     distance: pos.distance,
-        //     offset: pos.offset,
-        //     velocity: 200
-        // })
+    function onMouseEnter() {
+        isMousePresent.current = true
     }
 
     function onMouseLeave() {
-        isDown.current = false
-        menu.current.style.cursor = "unset"
+        isMousePresent.current = false
     }
 
     return (
@@ -145,9 +159,10 @@ function Menu() {
             id='menu' 
             style={{transform: `translateX(${pos.offset}px)`}}
 
-            onMouseDown={onMouseDown}
-            onMouseUp={onMouseUp}
-            onMouseMove={onMouseMove}
+            // onMouseDown={onMouseDown}
+            // onMouseUp={onMouseUp}
+            // onMouseMove={onMouseMove}
+            onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
 
             // onTouchStart={onMouseDown}
