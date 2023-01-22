548a9e9bb8a828e4a98e4c175d25e97287c0e315
Josh Devine <joshuaddevine@gmail.com>
2023-01-22 14:13:23 -0500

    removed unused functions

--- src/pages/Lists.js
+++ src/pages/Lists.js
@@ -3,33 +3,33 @@ import styles from './Lists.module.css'
 
 import Arrow from './Arrow';
 
-const CATEGORIES = [
-    { name: 'Music', slug: 'music' },
-    { name: 'Movies', slug: 'movies' },
-    { name: 'Art (Museums)', slug: 'art' },
-    { name: 'Songs of the Month', slug: 'songs' },
-    { name: 'Literature', slug: 'literature' },
-    { name: 'Television', slug: 'tv' },
-    { name: 'Lists', slug: '' },
-    { name: 'Memes', slug: '' },
-    { name: 'People', slug: '' },
-    { name: 'Write for Us', slug: '' },
-]
+// const CATEGORIES = [
+//     { name: 'Music', slug: 'music' },
+//     { name: 'Movies', slug: 'movies' },
+//     { name: 'Art (Museums)', slug: 'art' },
+//     { name: 'Songs of the Month', slug: 'songs' },
+//     { name: 'Literature', slug: 'literature' },
+//     { name: 'Television', slug: 'tv' },
+//     { name: 'Lists', slug: '' },
+//     { name: 'Memes', slug: '' },
+//     { name: 'People', slug: '' },
+//     { name: 'Write for Us', slug: '' },
+// ]
 
-function Category({name, slug}, index) {
-    return (
-        <h2 key={index}>
-            <Link 
-                to={slug}
-                reloadDocument
-                id={styles[slug]}
-                className={styles.link}
-            >
-                {name}
-            </Link>
-        </h2>
-    )
-}
+// function Category({name, slug}, index) {
+//     return (
+//         <h2 key={index}>
+//             <Link 
+//                 to={slug}
+//                 reloadDocument
+//                 id={styles[slug]}
+//                 className={styles.link}
+//             >
+//                 {name}
+//             </Link>
+//         </h2>
+//     )
+// }
 
 function Lists() {
     // let items = CATEGORIES.map(Category)
