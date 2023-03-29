a3fc258f06e25991f905209e8466788e8a70460d
Josh Devine <joshuaddevine@gmail.com>
2023-03-28 21:27:42 -0400

    create museums page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Explorer from './pages/Explorer'
 import Songs from './pages/Songs'
+import Museums from './pages/Museums/Museums'
 import Lists from './pages/Lists'
 import Review from './pages/Review'
 
 function App() {
     return (
         <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/songs' element={<Songs />} />
-            <Route exact path='/lists' element={<Lists />} />
+            <Route path='/' element={<Home />} />
+            <Route path='songs' element={<Songs />} />
+            <Route path='lists' element={<Lists />} />
+            <Route path='museums/:slug' element={<Museums />} />
+            <Route path='museums/:slug/:exhibit' element={<Review />} />
             <Route path=':category' element={<Explorer />} />
             <Route path=':category/:slug' element={<Review />} />
         </Routes>
--- src/CollectionLoader.js
+++ src/CollectionLoader.js
@@ -10,6 +10,7 @@ export const CollectionLoader = {
     music: () => require.context('/content/music', true, /.json$/).keys().map((key) => extract('music', key)),
     movies: () => require.context('/content/movies', true, /.json$/).keys().map((key) => extract('movies', key)),
     art: () => require.context('/content/art', true, /.json$/).keys().map((key) => extract('art', key)),
+    museums: () => require.context('/content/museums', true, /.json$/).keys().map((key) => extract('museums', key)),
     songs: () => require.context('/content/songs', true, /.json$/).keys().map((key) => extract('songs', key)),
     literature: () => require.context('/content/literature', true, /.json$/).keys().map((key) => extract('literature', key)),
     tv: () => require.context('/content/tv', true, /.json$/).keys().map((key) => extract('tv', key)),
